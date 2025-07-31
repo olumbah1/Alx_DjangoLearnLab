2bdd3799484863671aaf645676cb1de0d58aa7d9
Olumide <adeoyeolumbah@gmail.com>
2025-07-31 21:10:03 +0100

    Created a class Book model

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BookListAPIView
 
 urlpatterns = [
-    path('Book/', BookListAPIView.as_view(), name = 'Book = book-list'),
+    path('book/', BookListAPIView.as_view(), name = 'book list'),
     
 ]
