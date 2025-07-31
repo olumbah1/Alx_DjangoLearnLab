7e0502d64521fcdf5a37b656aa1864ba5994ba9c
Olumide <adeoyeolumbah@gmail.com>
2025-08-01 00:48:17 +0100

    Created an authentication, permissions and IsAdmin

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from .views import BookViewSet,BookList
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+        path('api/token/', obtain_auth_token, name='api_token_auth'),
      # Route for the BookList view (ListAPIView)
     path('books/', BookList.as_view(), name='book-list'),
     
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,29 @@
-from rest_framework import generics,viewsets
+from rest_framework import generics,viewsets,status
 from .serializers import BookSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 from .models import Book
 
-# Create your views here.
+
+# List all books – accessible by authenticated users
 class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated] 
+    serializer_class = BookSerializer  
+    queryset = Book.objects.all()      
+
+
+# ViewSet for admin users to manage books (CRUD)
 class BookViewSet(viewsets.ModelViewSet):
-     queryset = Book.objects.all()
-     serializer_class = BookSerializer
-    
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)  
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+     
