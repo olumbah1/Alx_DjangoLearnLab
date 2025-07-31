c7d0922b6fcd167282409239ab8d3746cf4123d6
Olumide <adeoyeolumbah@gmail.com>
2025-07-31 21:17:55 +0100

    Created a class Book model

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import BookListAPIView
+from .views import BookList
 
 urlpatterns = [
-    path('book/', BookListAPIView.as_view(), name = 'book list'),
+    path('books/', BookList.as_view(), name='book-list'),
     
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.generics import ListAPIView
 from .serializers import BookSerializer
 from .models import Book
 # Create your views here.
-class BookListAPIView(generics.ListAPIView):
+class BookList(ListAPIView):
     queryset = Book.object.all()
     serializer_class = BookSerializer
     
-    def perform_create(self, serializer):
-        serializer.save()
