ef63556631f1b40a89aefd01a0c4cb06dd6321d9
Olumide <adeoyeolumbah@gmail.com>
2025-08-05 17:15:41 +0100

    created a CRUD operations and added permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from rest_framework.response import Response
 from .models import Book, Author
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 # Create your views here.
