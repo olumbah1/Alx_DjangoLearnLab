a5aff29c0542b5a0ee0b2f390e59324f31771519
Olumide <adeoyeolumbah@gmail.com>
2025-08-05 18:09:45 +0100

    Added filter, searchfilter, ordering into the Book model view

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from .models import Book, Author
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework, DjangoFilterBackend
 
 
 # Create your views here.
