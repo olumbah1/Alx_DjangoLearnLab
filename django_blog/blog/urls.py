ccce28d6ef95266f8600e427529c77ec1b893372
Olumide <adeoyeolumbah@gmail.com>
2025-08-15 17:59:17 +0100

    Made little adjustment to the url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     
     # Comment CRUD Views URL.
-    path('post/<int:pk>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
