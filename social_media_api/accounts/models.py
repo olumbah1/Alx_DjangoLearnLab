c48231336536a4324dec9ef16f19b46c2b5eeb47
Olumide <adeoyeolumbah@gmail.com>
2025-08-19 15:27:55 +0100

    Created a Custom user and accounts views

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
-    profile_profile = models.ImageField(upload_to='Images', blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='Images', blank=True, null=True)
     followers = models.ManyToManyField(
         'self',
         symmetrical=False,
